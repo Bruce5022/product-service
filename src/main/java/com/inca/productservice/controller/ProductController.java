34a003e3728acfd076a61528e4b9d5cf9033cdd4
史战伟 <shizhanwei123@126.com>
2018-10-11 07:36:38 +0800

    完善商品服务代码
    Signed-off-by: 史战伟 <shizhanwei123@126.com>

--- src/main/java/com/inca/productservice/controller/ProductController.java
+++ src/main/java/com/inca/productservice/controller/ProductController.java
@@ -1,11 +1,28 @@
 package com.inca.productservice.controller;
 
+import com.inca.productservice.entity.Product;
+import com.inca.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
 
+    @RequestMapping("/list")
+    public List<Product> list() {
+        return productService.listProduct();
+    }
+
+    @RequestMapping("/findOne")
+    public Product findOne(@RequestParam("id") Integer id) {
+        return productService.findOne(id);
+    }
 }
--- src/main/java/com/inca/productservice/entity/Product.java
+++ src/main/java/com/inca/productservice/entity/Product.java
@@ -1,4 +1,53 @@
 package com.inca.productservice.entity;
 
-public class Product {
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+    private Integer id;
+    private String name;
+    private Integer price;
+    private Integer stock;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, Integer price, Integer stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
 }
