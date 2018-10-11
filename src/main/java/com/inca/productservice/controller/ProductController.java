2e31de9e7d3af81fa142dd6b2d717cc3b485faae
史战伟 <shizhanwei123@126.com>
2018-10-12 05:48:33 +0800

    多节点数据区分端口号
    Signed-off-by: 史战伟 <shizhanwei123@126.com>

--- src/main/java/com/inca/productservice/controller/ProductController.java
+++ src/main/java/com/inca/productservice/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.inca.productservice.controller;
 import com.inca.productservice.entity.Product;
 import com.inca.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +14,25 @@ import java.util.List;
 @RequestMapping("/api/product")
 public class ProductController {
 
+    @Value("${server.port}")
+    private String port;
+
     @Autowired
     private ProductService productService;
 
     @RequestMapping("/list")
     public List<Product> list() {
-        return productService.listProduct();
+        List<Product> products = productService.listProduct();
+        for (Product pro : products) {
+            pro.setName(port + ":" + pro.getName());
+        }
+        return products;
     }
 
     @RequestMapping("/findOne")
     public Product findOne(@RequestParam("id") Integer id) {
-        return productService.findOne(id);
+        Product product = productService.findOne(id);
+        product.setName(port + ":" + product.getName());
+        return product;
     }
 }
